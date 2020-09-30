f552b9b0de2686bf2f6de85bbd35897e239758f3
Cody-Blair <cb.rialbc@gmail.com>
2020-09-30 12:47:12 -0600

    first big upload

--- main.js
+++ main.js
@@ -11,18 +11,46 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+const user1Victory = "User 1 Wins!"
+
+const user2Victory = "User 2 Wins!"
+
+const drawGame = "It's a draw!"
+
 // the function that will be called by the unit test below
 const rockPaperScissors = (hand1, hand2) => {
 
-  // Write code here
-  // Use the unit test to see what is expected
+  hand1 = hand1.trim()
+  hand2=hand2.trim()
+  
+  if ((hand1 === "rock")&&(hand2 ==="paper")){ 
+    console.log(user2Victory)
+  } else if ((hand1 === "rock")&&(hand2 === "scissors")) {
+    console.log(user1Victory)
+  } else if ((hand1 === "paper")&&(hand2 === "scissors")){
+    console.log(user2Victory)
+  } else if ((hand1 === "paper")&&(hand2 === "rock")) {
+    console.log(user1Victory)
+  } else if ((hand1 === "scissors")&&(hand2 === "paper")) {
+    console.log(user1Victory)
+  } else if ((hand1 === "scissors")&&(hand2 === "rock")) {
+    console.log(user2Victory)
+  } else if ((hand1 === "rock")&&(hand2 === "rock")){
+    console.log(drawGame)
+  } else if ((hand1 === "paper")&&(hand2 === "paper")){
+    console.log(drawGame)
+  } else if ((hand1 === "scissors")&&(hand2 === "scissors")){
+    console.log(drawGame)
+} else {
+  console.log("Input invalid. Try 'rock' 'paper' or 'scissors'")
+}
 
 }
 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
 // to close it ctrl + C
-function getPrompt() {
+const getPrompt= () => {
   rl.question('hand1: ', (answer1) => {
     rl.question('hand2: ', (answer2) => {
       console.log( rockPaperScissors(answer1, answer2) );
@@ -31,32 +59,6 @@ function getPrompt() {
   });
 }
 
-// Unit Tests
-// You use them run the command: npm test main.js
-// to close them ctrl + C
-if (typeof describe === 'function') {
-
-  // most are notes for human eyes to read, but essentially passes in inputs then compares if the function you built return the expected output.
-  describe('#rockPaperScissors()', () => {
-    it('should detect a tie', () => {
-      assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
-      assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
-      assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
-    });
-    it('should detect which hand won', () => {
-      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
-      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
-    });
-    it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
-      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
-    });
-  });
-} else {
-
   // always returns ask the user for another input
-  getPrompt();
+  getPrompt()
 
-}
